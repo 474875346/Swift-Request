6ec601da5e0bd7bc444c68e6f57bbedf802fee64
474875346 <474875346@qq.com>
2016-12-23 10:58:48 +0800

    修改

--- Swift-Request/ViewController.swift
+++ Swift-Request/ViewController.swift
@@ -22,10 +22,8 @@ class ViewController: UIViewController {
         //            error  in
         //            print(error!)
         //        })
-        let image = UIImage.init(named: "566a60db03ffa.jpg")!
-        let url = "https://172.16.1.5:8443/api/voucher/uploadForIOS"
-        
-        
+//        let image = UIImage.init(named: "566a60db03ffa.jpg")!
+//        let url = "https://172.16.1.5:8443/api/voucher/uploadForIOS"
         //        Alamofire.upload(imageData, to: "http://101.201.53.181:8088/user/avatar", method: .post, headers: ["Authorization":"e654da84-cf65-4111-a81e-95049155309f"]).responseJSON { response in
         //            if response.result.isSuccess {
         //               print(response.result.value as Any)
@@ -33,14 +31,14 @@ class ViewController: UIViewController {
         //                print(response.result.error as Any)
         //            }
         //        }
-        let param = ["app_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJfaWQiOjUxLCJleHQiOjE0ODE1MDk1ODQ3NTQsImlhdCI6MTQ4MTUwNTk4NDc1NCwiY2xpZW50X2lkIjoiQ0ZDNTAwNjUtNjg4Ni00Q0Q4LTgwQjgtNDQzNTNFRjM1RDA5In0.2lMm1posezVnVQvQHh22qxFyER4BkrX_cIJ1pb5wlug","client":"CFC50065-6886-4CD8-80B8-44353EF35D09","voucherTag":"哈哈"]
-        HttpRequestTool.sharedInstance.HttpRequestUpload(url: url, parameters: param, image: image, successed: {
-            success in
-            print(success as Any)
-        }, failed: {
-            failed in
-            print(failed as Any)
-        })
+//        let param = ["app_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJfaWQiOjUxLCJleHQiOjE0ODE1MDk1ODQ3NTQsImlhdCI6MTQ4MTUwNTk4NDc1NCwiY2xpZW50X2lkIjoiQ0ZDNTAwNjUtNjg4Ni00Q0Q4LTgwQjgtNDQzNTNFRjM1RDA5In0.2lMm1posezVnVQvQHh22qxFyER4BkrX_cIJ1pb5wlug","client":"CFC50065-6886-4CD8-80B8-44353EF35D09","voucherTag":"哈哈"]
+//        HttpRequestTool.sharedInstance.HttpRequestUpload(url: url, parameters: param, image: image, successed: {
+//            success in
+//            print(success as Any)
+//        }, failed: {
+//            failed in
+//            print(failed as Any)
+//        })
         
         
         
